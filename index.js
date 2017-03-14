607094e3ce20d677d05802002da396ada125db1d
Rocky Madden <git@rockymadden.com>
2017-03-14 11:48:45 -0600

    Baseline implementation

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const {join} = require('path');
+const {sync: exists} = require('path-exists');
+const {
+	T, allPass, always, apply, complement, cond, defaultTo, ifElse, is, isEmpty, isNil, nthArg, pipe, toPairs
+} = require('ramda');
+const {create, env} = require('sanctuary');
+
+const {Left, map, Right} = create({checkTypes: false, env});
+
+const isNotEmpty = complement(isEmpty);
+const isNotNil = complement(isNil);
+
+const validOptions = allPass([isNotNil, is(Object)]);
+const validPath = exists;
+const validScript = allPass([isNotNil, isNotEmpty, is(String)]);
+
+const parseOption = (key, value) => `${key}:${value}`;
+const parseOptions = pipe(
+	toPairs,
+	map(apply(parseOption)),
+	opts => opts.join(' '),
+	ifElse(
+		isEmpty,
+		always(''),
+		opts => ` -- ${opts}`
+	)
+);
+
+const exec = (shell, prefix) => (pkg, script, options = {}) => {
+	const path = join(defaultTo('.', prefix), 'node_modules', pkg);
+
+	const validate = cond([
+		[pipe(nthArg(0), complement(validPath)), always(Left(new Error('Invalid path')))],
+		[pipe(nthArg(1), complement(validScript)), always(Left(new Error('Invalid script')))],
+		[pipe(nthArg(2), complement(validOptions)), always(Left(new Error('Invalid options')))],
+		[T, (path, script, opts) => Right(`cd ${path} && npm run ${script}${parseOptions(opts)}`)]
+	]);
+
+	return pipe(
+		validate,
+		map(shell)
+	)(path, script, options);
+};
+
+module.exports = (shell, prefix) => exec(shell, prefix);
---
+++ test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const test = require('ava');
+const {shell} = require('execa');
+const {create, env} = require('sanctuary');
+const lope = require('.')(shell);
+
+const {fromEither, either} = create({checkTypes: false, env});
+
+test.serial('invalid package should return Left', t => either(
+	err => t.is(err.message, 'Invalid path'),
+	() => t.fail()
+)(lope('invalid', 'true')));
+
+test.serial('invalid script should return Left', t => either(
+	err => t.is(err.message, 'Invalid script'),
+	() => t.fail()
+)(lope('lope-example', 0)));
+
+test.serial('invalid options should return Left', t => either(
+	err => t.is(err.message, 'Invalid options'),
+	() => t.fail()
+)(lope('lope-example', 'true', 0)));
+
+test.serial('valid arguments should return Right', t => either(
+	() => t.fail(),
+	() => t.pass()
+)(lope('lope-example', 'true')));
+
+test.serial('valid and correct arguments awaited should return successful execa execution', async t => {
+	const executed = await fromEither(null)(lope('lope-example', 'true'));
+
+	t.is(executed.code, 0);
+});
+
+test.serial('valid and correct arguments including options awaited should return successful execa execution', async t => {
+	const executed = await fromEither(null)(lope('lope-example', 'echo', {echo: 'hello'}));
+
+	t.true(executed.stdout.indexOf('hello') >= 0);
+});
+
+test.serial('valid but incorrect arguments awaited should return failed execa execution', async t => {
+	try {
+		await fromEither(null)(lope('lope-example', 'false'));
+		t.fail();
+	} catch (err) {
+		t.not(err.code, 0);
+	}
+});
